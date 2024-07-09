d55976b37082dae0226f2a9dd1ac5998b87e2c34
Galushko Evgeniy <yudginchik@gmail.com>
2024-07-09 10:59:45 +0300

    add console file

--- js/task-1.js
+++ js/task-1.js
@@ -10,16 +10,11 @@ function makeTransaction (quantity, pricePerDroid) {
   console.log(textTheOrder)
 }
 
-// function makeTransaction (quantity, pricePerDroid) {
-//   const textTheOrder = `You ordered ${quantity} droids worth ${quantity * pricePerDroid} credits!`
-//   return textTheOrder;
-//   console.log(textTheOrder)
-// }
 
 
-makeTransaction( 5 , 3000 );  
-makeTransaction( 3 , 1000 );  
-makeTransaction( 10 , 500 );
+console.log(makeTransaction(5, 3000));  
+console.log(makeTransaction(3, 1000));  
+console.log(makeTransaction(10, 500));
 
 
 
